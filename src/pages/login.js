2e2a5891c5855337b9876c519275045e02fa82d2
lucasgballesta <112784286+lucasgballesta@users.noreply.github.com>
2024-09-16 21:01:39 -0300

    login y register

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,64 @@
 // pages/Login.js
 import React from 'react';
+import { Container, Typography, TextField, FormControlLabel, Checkbox, Button, Box } from '@mui/material';
 
 function Login() {
-  return <h1>Iniciar Sesión, yes</h1>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography variant="h5">Iniciar Sesión</Typography>
+        <Box
+          component="form"
+          noValidate
+          sx={{
+            mt: 3,
+            width: '100%',
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default Login;
--- src/pages/registro.js
+++ src/pages/registro.js
@@ -1,8 +1,64 @@
 // pages/Registro.js
 import React from 'react';
+import { Container, Typography, TextField, FormControlLabel, Checkbox, Button, Box } from '@mui/material';
 
 function Registro() {
-  return <h1>Registro de Cuenta</h1>;
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography variant="h5">¿Primera vez? ¡Registrate!</Typography>
+        <Box
+          component="form"
+          noValidate
+          sx={{
+            mt: 3,
+            width: '100%',
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default Registro;
